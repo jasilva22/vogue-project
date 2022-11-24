fe482b2a1f99d4f41d73c643c44e93bb1d2dbbba
J Silva <34302180+jasilva22@users.noreply.github.com>
2022-11-23 21:44:00 -0800

    bug fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ module.exports = {
   content: [
     './_includes/**/*.{html,md}',
     './_layouts/**/*.{html,md}',
-    './_*/*.{html,md}',
+    './_*/**/*.{html,md}',
+    '!./_site/**/*.{html,md}',
     './*.{html,md}'
   ],
   theme: {
